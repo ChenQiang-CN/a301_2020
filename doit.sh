3df03bc1cd112193d5b7537f59cc210b352b18c6
Phil Austin <you@example.comw>
2020-10-22 09:56:33 -0700

    add figures

--- doit.sh
+++ doit.sh
@@ -2,3 +2,4 @@
 jb build notebooks
 cp -a notebooks/week6/images notebooks/_build/html/week6/.
 cp -a notebooks/week6/figures notebooks/_build/html/week6/.
+cp -a notebooks/week6/answers/figures notebooks/_build/html/week6/answers/.
