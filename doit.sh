cfe38dcff50ab836c1ab99d9e8861553794331ff
Phil Austin <paustin@eos.ubc.ca>
2020-11-16 09:32:26 -0800

    monday

--- doit.sh
+++ doit.sh
@@ -5,3 +5,4 @@ cp -a notebooks/week6/figures notebooks/_build/html/week6/.
 cp -a notebooks/week6/answers/figures notebooks/_build/html/week6/answers/.
 cp -a notebooks/week9/figures notebooks/_build/html/week9/.
 cp -a notebooks/week10/figures notebooks/_build/html/week10/.
+cp -a notebooks/week11/figures notebooks/_build/html/week11/.
